84837505e000fff1714c59acc974de121e667a25
jjcarlospv <jpaucarm6@gmail.com>
2015-05-26 14:13:17 -0500

    Aumenté dos marcadores y cambie la apariencia del mapa

---
+++ app/src/main/java/me/doapps/bombergis/MainActivity.java
@@ -0,0 +1,76 @@
+package me.doapps.bombergis;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.ZoomControls;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * Created by user on 26/05/2015.
+ */
+public class MainActivity extends ActionBarActivity {
+    private GoogleMap map;
+    UiSettings mapSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setUpMapIfNeeded();
+    }
+
+    private void setUpMapIfNeeded() {
+        if (map == null) {
+            map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+            if (map != null) {
+                setUpMap();
+            }
+        }
+    }
+
+    private void setUpMap() {
+        map.setMyLocationEnabled(true);
+        map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        map.getUiSettings().setZoomControlsEnabled(true);
+         //mapSettings = map.getUiSettings();
+        map.getUiSettings().setScrollGesturesEnabled(true);
+        map.getUiSettings().setTiltGesturesEnabled(true);
+        map.getUiSettings().setRotateGesturesEnabled(true);
+
+        /*Marker marker = map.addMarker(new MarkerOptions()
+                .position(new LatLng(-12.1023776, -77.0219219))
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));*/
+        Marker marker = map.addMarker(new MarkerOptions()
+                .position(new LatLng(-12.1023776,-77.0219219))
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_marker)));
+
+        Marker marker2 = map.addMarker(new MarkerOptions()
+        .position(new LatLng(-12.1021255,-77.0219215))
+        .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_marker2)));
+
+        CameraPosition camPos = new CameraPosition.Builder()
+                .target(new LatLng(-12.1023776,-77.0219219))
+                .zoom(16)
+                .build();
+        CameraUpdate camUpd3 = CameraUpdateFactory.newCameraPosition(camPos);
+        map.animateCamera(camUpd3);
+    }
+
+
+
+
+}
