e32b9034d82114bd1f316010c8871a947b1db667
angraaquaria <miguelgarrafag@gmail.com>
2015-05-26 15:08:57 -0500

    se coloco iconos a los botones

--- app/src/main/java/me/doapps/bombergis/MainActivity.java
+++ app/src/main/java/me/doapps/bombergis/MainActivity.java
@@ -51,16 +51,30 @@ public class MainActivity extends ActionBarActivity {
         map.getUiSettings().setTiltGesturesEnabled(true);
         map.getUiSettings().setRotateGesturesEnabled(true);
 
-        /*Marker marker = map.addMarker(new MarkerOptions()
-                .position(new LatLng(-12.1023776, -77.0219219))
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));*/
         Marker marker = map.addMarker(new MarkerOptions()
+                .position(new LatLng(-12.1023776, -77.0219219))
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        Marker marker1 = map.addMarker(new MarkerOptions()
                 .position(new LatLng(-12.1023776,-77.0219219))
                 .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_marker)));
-
         Marker marker2 = map.addMarker(new MarkerOptions()
-        .position(new LatLng(-12.1021255,-77.0219215))
-        .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_marker2)));
+                .position(new LatLng(-12.1023762, -77.0219762))
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_gasstation)));
+        Marker marker3 = map.addMarker(new MarkerOptions()
+                .position(new LatLng(-12.1023734,-77.0219975))
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_fireman)));
+        Marker marker4 = map.addMarker(new MarkerOptions()
+                .position(new LatLng(-12.1023896,-77.0219146))
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_comisaria)));
+        Marker marker5 = map.addMarker(new MarkerOptions()
+                .position(new LatLng(-12.1023245,-77.0219985))
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hospital)));
+        Marker marker6 = map.addMarker(new MarkerOptions()
+                .position(new LatLng(-12.1023065,-77.0218354))
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.hidrante)));
+        Marker marker7 = map.addMarker(new MarkerOptions()
+                .position(new LatLng(-12.1021556,-77.0212456))
+                .icon(BitmapDescriptorFactory.fromResource(R.mipmap.colegio)));
 
         CameraPosition camPos = new CameraPosition.Builder()
                 .target(new LatLng(-12.1023776,-77.0219219))
