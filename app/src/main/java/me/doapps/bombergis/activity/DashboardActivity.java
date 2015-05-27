adf4af744bb4a49de50d3ab0a9d15d3150e7667b
jjcarlospv <jpaucarm6@gmail.com>
2015-05-27 15:33:53 -0500

    Prueba de interface

--- app/src/main/java/me/doapps/bombergis/activity/DashboardActivity.java
+++ app/src/main/java/me/doapps/bombergis/activity/DashboardActivity.java
@@ -5,16 +5,24 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import me.doapps.bombergis.R;
 import me.doapps.bombergis.fragment.InterestFragment;
+import me.doapps.bombergis.fragment.MapsFragment;
+import me.doapps.bombergis.fragment.RouteFragment;
 import me.doapps.bombergis.fragment.SearchFragment;
 
 /**
@@ -23,7 +31,11 @@ import me.doapps.bombergis.fragment.SearchFragment;
 public class DashboardActivity extends ActionBarActivity implements View.OnClickListener {
     private ImageButton btnSearch;
     private ImageButton btnInterest;
+    private ImageButton btnMaps;
+    private ImageButton btnRoute;
     private GoogleMap map;
+
+    private List<LatLng> colegios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +49,24 @@ public class DashboardActivity extends ActionBarActivity implements View.OnClick
         btnSearch = (ImageButton) findViewById(R.id.btnSearch);
         btnSearch.setOnClickListener(this);
 
-        btnInterest= (ImageButton) findViewById(R.id.bntInterest);
+        btnInterest = (ImageButton) findViewById(R.id.btnInterest);
         btnInterest.setOnClickListener(this);
+
+        btnMaps = (ImageButton) findViewById(R.id.btnMaps);
+        btnMaps.setOnClickListener(this);
+
+        btnRoute = (ImageButton) findViewById(R.id.btnRoute);
+        btnRoute.setOnClickListener(this);
+
+ /*agregamos colegios*/
+        colegios = new ArrayList<>();
+        colegios.add(new LatLng(-12.106524, -77.025632));
+        colegios.add(new LatLng(-12.095632, -77.025623));
+        colegios.add(new LatLng(-12.156524, -77.054525));
+        colegios.add(new LatLng(-12.086424, -77.096524));
+        colegios.add(new LatLng(-12.164524, -77.046247));
+        colegios.add(new LatLng(-12.067241, -77.057324));
+
     }
 
 
@@ -72,15 +100,46 @@ public class DashboardActivity extends ActionBarActivity implements View.OnClick
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.btnSearch) {
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.containerLayout, new SearchFragment())
-                    .commit();
+
+        switch (v.getId()) {
+            case R.id.btnSearch:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.containerLayout, new SearchFragment())
+                        .commit();
+                break;
+
+            case R.id.btnInterest:
+                InterestFragment interestFragment = new InterestFragment();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.containerLayout, interestFragment)
+                        .commit();
+                interestFragment.setInterfaceInstitutes(new InterestFragment.InterfaceInstitutes() {
+                    @Override
+                    public void getInstitute(int flag) {
+                        if (flag == 1) {
+                            for (int i = 0; i < colegios.size(); i++) {
+                                map.addMarker(new MarkerOptions()
+                                        .position(colegios.get(i))
+                                        .icon(BitmapDescriptorFactory.fromResource(R.mipmap.ic_school)));
+                            }
+                        }
+                    }
+                });
+                break;
+
+            case R.id.btnMaps:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.containerLayout, new MapsFragment())
+                        .commit();
+                break;
+
+            case R.id.btnRoute:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.containerLayout, new RouteFragment())
+                        .commit();
+                break;
+
         }
-       if (v.getId()== R.id.bntInterest){
-           getSupportFragmentManager().beginTransaction()
-                   .replace(R.id.containerLayout, new InterestFragment())
-                   .commit();
-       }
+
     }
 }
--- app/src/main/java/me/doapps/bombergis/fragment/InterestFragment.java
+++ app/src/main/java/me/doapps/bombergis/fragment/InterestFragment.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import me.doapps.bombergis.R;
 
@@ -13,6 +16,9 @@ import me.doapps.bombergis.R;
  * Created by MiguelGarrafa_2 on 27/05/2015.
  */
 public class InterestFragment extends Fragment {
+    private Button btnTest;
+    private CheckBox checkHidrantes;
+    private InterfaceInstitutes interfaceInstitutes;
 
     public InterestFragment(){}
 
@@ -20,6 +26,32 @@ public class InterestFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_interest, container, false);
+        btnTest = (Button) view.findViewById(R.id.btnTest);
+        checkHidrantes = (CheckBox) view.findViewById(R.id.checkHidrantes);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        checkHidrantes.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    interfaceInstitutes.getInstitute(1);
+                }
+            }
+        });
+
+
+    }
+
+    /*inner interface*/
+    public interface InterfaceInstitutes{
+        void getInstitute(int flag);
+    }
+    public void setInterfaceInstitutes(InterfaceInstitutes interfaceInstitutes){
+        this.interfaceInstitutes = interfaceInstitutes;
     }
 }
