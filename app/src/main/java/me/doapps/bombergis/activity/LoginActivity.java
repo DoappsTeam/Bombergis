85a87efbb248c8cb82d10d7be98d3884534ba6c3
jjcarlospv <jpaucarm6@gmail.com>
2015-05-28 10:24:48 -0500

    Corrección de string en layout Login

--- app/src/main/java/me/doapps/bombergis/activity/LoginActivity.java
+++ app/src/main/java/me/doapps/bombergis/activity/LoginActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import me.doapps.bombergis.R;
 
@@ -12,12 +14,20 @@ import me.doapps.bombergis.R;
  */
 public class LoginActivity extends ActionBarActivity {
 
+    TextView txtUser;
+    TextView txtPass;
+    Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtUser = (TextView)findViewById(R.id.txtUser);
+        txtPass = (TextView)findViewById(R.id.txtPass);
+        btnLogin = (Button)findViewById(R.id.btnLogin);
     }
-    public void ingresar(View view) {
+    public void ingresarCuenta(View view) {
         Intent i = new Intent(this, DashboardActivity.class);
         startActivity(i);
     }
